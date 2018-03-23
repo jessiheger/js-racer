62d7312238a90de14da9bc8d5afdf29ad3f08f00
jessiheger <jhmheger@gmail.com>
2018-03-23 15:44:28 -0700

    add files

---
+++ racer.app.js
@@ -0,0 +1,5 @@
+// create two players
+// line them up on the left-hand side of screen
+// assign specific keys for each player
+// use those keys to move players across the screen
+// when one crosses finish line, alert: Player 1/2 wins!
