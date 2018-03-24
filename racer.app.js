53ce28aeb60fd94f77e9adfe1851acd11028d26a
jessiheger <jhmheger@gmail.com>
2018-03-24 15:34:11 -0700

    alert still occurs one keypress after finish line

--- racer.app.js
+++ racer.app.js
@@ -1,35 +1,57 @@
 const square = document.getElementById('carOne');
 const square2 = document.getElementById('carTwo');
 
-
-let pos = 200;
+let pos1 = 150;
+let pos2 = 150;
 
 // const moveAlong = (e) => {
     document.addEventListener('keydown', (e) => {
-        if (70 === e.keyCode && pos < 1100){
-            pos += 100;
-            square.style.left = pos + 'px';
+        if (70 === e.keyCode) {
+            if (pos1 < 1100){
+            pos1 += 100;
+            square.style.left = pos1 + 'px';
         }
-        else {
-            if (70 === e.keyCode && pos === 1100){
-                square.style.left = 1100 + 'px';
-                alert('Player1 wins!');
-            }
-        }
-});
-    document.addEventListener('keydown', (e) => {
-        if (74 === e.keyCode && pos < 1100){
-            pos += 100;
-            square2.style.left = pos + 'px';
-        }
-        else {
-            if (74 === e.keyCode && pos === 1100){
-                square2.style.left = 1100 + 'px';
-                alert('Player2 wins!');
+            else {
+                if (pos1 >= 1050){
+                alert('Player1 wins!'); 
+            // square.style.left = 1100 + 'px';
+             }
         }
     }
+    if (74 === e.keyCode) {
+        if (pos2 < 1100){
+        pos2 += 100;
+        square2.style.left = pos2 + 'px';
+    }
+        else { 
+            if (pos2 >= 1050){
+        // square2.style.left = 1100 + 'px';
+        alert('Player2 wins!'); 
+    }
+}
+}
 });
-// moveAlong(e);
+
+//         else {
+//             if (70 === e.keyCode && pos === 1100){
+//                 square.style.left = 1100 + 'px';
+//                 alert('Player1 wins!');
+//             }
+//         }
+// });
+//     document.addEventListener('keydown', (e) => {
+//         if (74 === e.keyCode && pos < 1100){
+//             pos += 100;
+//             square2.style.left = pos + 'px';
+//         }
+//         else {
+//             if (74 === e.keyCode && pos === 1100){
+//                 square2.style.left = 1100 + 'px';
+//                 alert('Player2 wins!');
+//         }
+//     }
+// });
+// // moveAlong(e);
 
 
 
