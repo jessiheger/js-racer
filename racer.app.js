aa49065ef5897aa72d3fcad09f476ea414a517ee
jessiheger <jhmheger@gmail.com>
2018-03-24 11:17:00 -0700

    event listener not working with button

--- racer.app.js
+++ racer.app.js
@@ -1,5 +1,40 @@
-// create two players
-// line them up on the left-hand side of screen
-// assign specific keys for each player
-// use those keys to move players across the screen
-// when one crosses finish line, alert: Player 1/2 wins!
+// const playerOne = document.getElementById('carOne');
+// const playerTwo = document.getElementById('carTwo');
+
+// let e;
+// const moveAlong = (e) => {
+//     playerOne.addEventListener("keydown", () => {
+//         if (e.keyCode === 70){
+//         carOne.setAttribute("style", "left: +=5"+ "px");
+//         }
+//     playerTwo.addEventListener("keydown", () => {
+//         if (e.keyCode === 74){
+//         carTwo.setAttribute("style", "left: +=5"+ "px");
+//         }
+       
+// })
+// })
+// };
+
+// const button = document.getElementById('goButton');
+const square = document.getElementById('carOne');
+//  DONT KNOW WHAT THIS DOES YET let pos = 0;
+
+
+// let e;
+// const moveAlong = (e) => {
+    goButton.addEventListener("click", () => {
+        square.style.left += 50 + 'px';
+        //  NOT WORKING: square.setAttribute("style", "left += 500 + 'px'");
+        // square.setAttribute("style", "background-color: yellow");
+    })
+// }
+
+
+
+
+
+
+
+// j key is 74
+// f key is 70
