176471bb6a02ec79e03bc35e298e79706082f10c
jessiheger <jhmheger@gmail.com>
2018-03-25 20:16:01 -0700

    final commit

--- racer.app.js
+++ racer.app.js
@@ -5,15 +5,21 @@ const button = document.getElementById('resetButton');
 // const turtlePic = document.getElementById('carOne');
 // turtlePic.setAttribute('src', 'images/turtle.png');
 
-
-
-
 let pos1 = 150;
 let pos2 = 150;
 
+
+button.addEventListener("click", (resetButton) => {
+    // pos1 = 150;
+    // pos2 = 150;
+    square.style.left = 150 + 'px';
+    square2.style.left = 150 + 'px';
+});
+
+
 // const moveAlong = (e) => {
     document.addEventListener('keydown', (e) => {
-        if (70 === e.keyCode) {
+        if (74 === e.keyCode) {
             if (pos1 < 1100){
             pos1 += 100;
             square.style.left = pos1 + 'px';
@@ -25,7 +31,7 @@ let pos2 = 150;
              }
         }
     }
-    if (74 === e.keyCode) {
+    if (70 === e.keyCode) {
         if (pos2 < 1100){
         pos2 += 100;
         square2.style.left = pos2 + 'px';
@@ -39,10 +45,4 @@ let pos2 = 150;
 }
 });
 
-button.addEventListener("click", (resetButton) => {
-    // pos1 = 150;
-    // pos2 = 150;
-    square.style.left = 150 + 'px';
-    square2.style.left = 150 + 'px';
-});
 
